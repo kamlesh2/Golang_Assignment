42ac27feda3ffda8c36b22eac560c8b04cebdcb2
Johni Sigh <johnisign@example.com>
2023-05-20 14:26:27 +0530

    get data in backend

--- go.mod
+++ go.mod
@@ -3,15 +3,31 @@ module example.com/hello
 go 1.20
 
 require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/certifi/gocertifi v0.0.0-20210507211836-431795d63e8d // indirect
 	github.com/edgedb/edgedb-go v0.13.6 // indirect
+	github.com/gofiber/fiber/v2 v2.46.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
+	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
 	github.com/sigurn/crc16 v0.0.0-20211026045750-20ab5afb07e3 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.47.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xdg/scram v1.0.5 // indirect
 	github.com/xdg/stringprep v1.0.3 // indirect
-	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.12 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,18 +3,81 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"fmt"
 )
+
+import "github.com/gofiber/fiber/v2"
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+
+// 	"github.com/edgedb/edgedb-go"
+// )
 
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates./*.gohtml"))
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
+// func main() {
+// 	http.HandleFunc("/", index)
+// 	http.HandleFunc("/process", processor)
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/process", processor)
-	http.ListenAndServe(":8080", nil)
+    app := fiber.New()
+
+    // app.Get("/", func(c *fiber.Ctx) error {
+    //     return c.SendString("Hello, World 👋!")
+    // })
+	
+	// DB
+
+	// ctx := context.Background()
+    // client, err := edgedb.CreateClient(ctx, edgedb.Options{})
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
+    // defer client.Close()
+
+    // var (
+    //     users []struct {
+    //         ID   edgedb.UUID `edgedb:"id"`
+    //         FName string      `edgedb:"fname"`
+    //         LName string      `edgedb:"lname"`
+    //     }
+    // )
+
+    // query := "SELECT User{name} FILTER .age = <int64>$0"
+    // err = client.Query(ctx, query, &users, age)
+
+	// DB
+
+	app.Static("/", "./public") 
+	
+	app.Post("/form/submit", func(c *fiber.Ctx) error {
+		fname := c.FormValue("first")
+		last := c.FormValue("last")
+		dob := c.FormValue("dob")
+		Phone := c.FormValue("Phone")
+		email := c.FormValue("email")
+		file, err := c.FormFile("cv")
+		fmt.Printf("%s\n",fname)
+		fmt.Printf("%s\n",last)
+		fmt.Printf("%s\n",dob)
+		fmt.Printf("%s\n",Phone)
+		fmt.Printf("%s\n",email)
+		fmt.Printf("%s\n",file)
+		fmt.Printf("%s\n",err)
+		return c.SendString("I'm a POST request!")
+	})
+
+    app.Listen(":3000")
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
